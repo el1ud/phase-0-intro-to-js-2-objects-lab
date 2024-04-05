936598ceabedf22f9fdcdd5650f8e0e73543fac7
eliud wainaina <wainainaeliud7@gmail.com>
2024-04-04 17:06:24 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee={
+    name:"Eliud",
+    streetadress: "27 ruiru street"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   return  Object.assign({}, employee,{ [key]: value});
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee [key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee={...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
